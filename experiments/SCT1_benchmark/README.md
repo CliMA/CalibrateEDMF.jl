a182badeefc899008fd9ee02210de2581818a614
Ignacio Lopez Gomez <ilopezgo@caltech.edu>
2022-02-16 22:10:13 -0800

    Add SCT1 benchmark.

---
+++ experiments/SCT1_benchmark/README.md
@@ -0,0 +1,36 @@
+## Stratocumulus-to-Cumulus Transition Benchmark 1 (SCT1)
+
+Authors: Ignacio Lopez-Gomez
+
+Last updated : February 2022
+
+This directory contains a `config.jl` file defining the SCT1 benchmark, along with files enabling efficient calibration in parallel in the Caltech central cluster. The SCT1 benchmark is composed of:
+
+- A training set of 5 AMIP configurations, spanning locations from the California coast to Hawai'i. The simulations in the training set use as forcing conditions the October 2004-2008 climatology.
+
+- A validation set of 5 AMIP4K configurations, spanning locations from the coast of Peru to the tropics. The simulations in the validation set use as forcing conditions the July 2004-2008 climatology.
+
+This benchmark contains few simulations for tractability. It is meant to be a minimally working benchmark of subtropical cloud climatologies (training) and their evolution under a strong climate change scenario (validation).
+
+# How to run: SLURM HPC pipeline
+
+Navigate to `global_parallel`. The master bash script that starts the calibration process is `ekp_par_calibration.sbatch`, which takes as an argument the path to the config file.
+
+If you are on the Caltech Central Cluster, you can run the project by adding it to the schedule:
+
+  >> sbatch ekp_par_calibration.sbatch ../config.jl
+
+# Output
+
+While the simulations run, the results are dumped to a directory named *results_...* after every iteration of the calibration algorithm. The diagnostics are stored in NetCDF4 format in `Diagnostics.nc`, within the results folder. NetCDF files may be processed using python or julia. You may also access a human-readable version of the netCDF file in linux using `ncdump`:
+
+  >> ncdump Diagnostics.nc > name_of_human_readable_file
+
+# Benchmarking
+
+Results for the `mse_full_nn_mean` and the spread (from `mse_full_var`) are included in `moisture_deficit_closure`. When using EKI, `mse_full_nn_mean` should be equivalent to `mse_full_mean` or `mse_full_min` after >10 iterations due to ensemble collapse. This reference calibrates 7 entrainment-related parameters from Cohen et al (2020). The evolution of the learnable parameters is also included in the directory.
+
+Results after 50 iterations:
+
+- MSE_full_nn_mean (train) : ~ 3.24
+- MSE_full_nn_mean (validation) : ~ 4.7
---
+++ experiments/SCT1_benchmark/config.jl
@@ -0,0 +1,176 @@
+#= Custom calibration configuration file. =#
+
+using Distributions
+using StatsBase
+using LinearAlgebra
+using CalibrateEDMF
+using CalibrateEDMF.ModelTypes
+using CalibrateEDMF.DistributionUtils
+using CalibrateEDMF.ReferenceModels
+using CalibrateEDMF.ReferenceStats
+using CalibrateEDMF.LESUtils
+using CalibrateEDMF.TurbulenceConvectionUtils
+using CalibrateEDMF.ModelTypes
+src_dir = dirname(pathof(CalibrateEDMF))
+include(joinpath(src_dir, "helper_funcs.jl"))
+# Import EKP modules
+using JLD2
+using EnsembleKalmanProcesses
+using EnsembleKalmanProcesses.ParameterDistributions
+
+struct SCT1Train end
+struct SCT1Val end
+
+function get_config()
+    config = Dict()
+    # Flags for saving output data
+    config["output"] = get_output_config()
+    # Define regularization of inverse problem
+    config["regularization"] = get_regularization_config()
+    # Define reference used in the inverse problem 
+    config["reference"] = get_reference_config(SCT1Train())
+    # Define reference used for validation
+    config["validation"] = get_reference_config(SCT1Val())
+    # Define the parameter priors
+    config["prior"] = get_prior_config()
+    # Define the kalman process
+    config["process"] = get_process_config()
+    # Define the SCM static configuration
+    config["scm"] = get_scm_config()
+    return config
+end
+
+function get_output_config()
+    config = Dict()
+    config["outdir_root"] = pwd()
+    config["overwrite_scm_file"] = false # Flag for overwritting SCM input file
+    return config
+end
+
+function get_regularization_config()
+    config = Dict()
+    # Regularization of observations: mean and covariance
+    config["perform_PCA"] = true # Performs PCA on data
+    config["variance_loss"] = 1.0e-2 # Variance truncation level in PCA
+    config["normalize"] = true  # whether to normalize data by pooled variance
+    config["tikhonov_mode"] = "relative" # Tikhonov regularization
+    config["tikhonov_noise"] = 1.0e-6 # Tikhonov regularization
+    config["dim_scaling"] = true # Dimensional scaling of the loss
+
+    # Parameter regularization: L2 regularization with respect to prior mean
+    # Set to `nothing` or `0.0` to use prior covariance as regularizer.
+    # To turn off regularization, set config["process"]["augmented"] to false.
+    config["l2_reg"] = 1.0
+    return config
+end
+
+function get_process_config()
+    config = Dict()
+    config["N_iter"] = 50
+    config["N_ens"] = 15 # Must be 2p+1 when algorithm is "Unscented"
+    config["algorithm"] = "Inversion" # "Sampler", "Unscented", "Inversion"
+    config["noisy_obs"] = false # Choice of covariance in evaluation of y_{j+1} in EKI. True -> Γy, False -> 0
+    # Artificial time stepper of the EKI.
+    config["Δt"] = 1.0
+    # Whether to augment the outputs with the parameters for regularization
+    config["augmented"] = true
+    config["failure_handler"] = "sample_succ_gauss" #"high_loss" #"sample_succ_gauss"
+    return config
+end
+
+function get_reference_config(::SCT1Train)
+    config = Dict()
+
+    # AMIP data: October
+    cfsite_numbers = (17, 18, 20, 22, 23)
+    les_kwargs = (forcing_model = "HadGEM2-A", month = 10, experiment = "amip")
+    ref_dirs = [get_cfsite_les_dir(cfsite_number; les_kwargs...) for cfsite_number in cfsite_numbers]
+    suffixes = [get_gcm_les_uuid(cfsite_number; les_kwargs...) for cfsite_number in cfsite_numbers]
+
+    n_repeat = length(ref_dirs)
+    config["case_name"] = repeat(["LES_driven_SCM"], n_repeat)
+    # Flag to indicate whether reference data is from a perfect model (i.e. SCM instead of LES)
+    config["y_reference_type"] = LES()
+    config["Σ_reference_type"] = LES()
+    config["y_names"] =
+        repeat([["s_mean", "ql_mean", "qt_mean", "total_flux_qt", "total_flux_s", "u_mean", "v_mean"]], n_repeat)
+    config["y_dir"] = ref_dirs
+    config["scm_suffix"] = suffixes
+    config["scm_parent_dir"] = repeat(["scm_init"], n_repeat)
+    config["t_start"] = repeat([3.0 * 3600], n_repeat)
+    config["t_end"] = repeat([6.0 * 3600], n_repeat)
+    # Use full LES timeseries for covariance
+    config["Σ_t_start"] = repeat([-5.75 * 24 * 3600], n_repeat)
+    config["Σ_t_end"] = repeat([6.0 * 3600], n_repeat)
+    config["write_full_stats"] = false
+    return config
+end
+
+function get_reference_config(::SCT1Val)
+    config = Dict()
+
+    # AMIP4K data: July
+    cfsite_numbers = (3, 5, 8, 11, 14)
+    les_kwargs = (forcing_model = "HadGEM2-A", month = 7, experiment = "amip4K")
+    ref_dirs = [get_cfsite_les_dir(cfsite_number; les_kwargs...) for cfsite_number in cfsite_numbers]
+    suffixes = [get_gcm_les_uuid(cfsite_number; les_kwargs...) for cfsite_number in cfsite_numbers]
+
+    n_repeat = length(ref_dirs)
+    config["case_name"] = repeat(["LES_driven_SCM"], n_repeat)
+    # Flag to indicate whether reference data is from a perfect model (i.e. SCM instead of LES)
+    config["y_reference_type"] = LES()
+    config["Σ_reference_type"] = LES()
+    config["y_names"] =
+        repeat([["s_mean", "ql_mean", "qt_mean", "total_flux_qt", "total_flux_s", "u_mean", "v_mean"]], n_repeat)
+    config["y_dir"] = ref_dirs
+    config["scm_suffix"] = suffixes
+    config["scm_parent_dir"] = repeat(["scm_init"], n_repeat)
+    config["t_start"] = repeat([3.0 * 3600], n_repeat)
+    config["t_end"] = repeat([6.0 * 3600], n_repeat)
+    # Use full LES timeseries for covariance
+    config["Σ_t_start"] = repeat([-5.75 * 24 * 3600], n_repeat)
+    config["Σ_t_end"] = repeat([6.0 * 3600], n_repeat)
+    config["write_full_stats"] = false
+    return config
+end
+
+function get_prior_config()
+    config = Dict()
+    config["constraints"] = Dict(
+        # entrainment parameters
+        "entrainment_factor" => [bounded(0.0, 1.0)],
+        "detrainment_factor" => [bounded(0.0, 1.0)],
+        "turbulent_entrainment_factor" => [bounded(0.0, 10.0)],
+        "entrainment_smin_tke_coeff" => [bounded(0.0, 10.0)],
+        "updraft_mixing_frac" => [bounded(0.0, 1.0)],
+        "entrainment_scale" => [bounded(1.0e-6, 1.0e-2)],
+        "sorting_power" => [bounded(0.0, 4.0)],
+    )
+
+    # TC.jl prior mean
+    config["prior_mean"] = Dict(
+        # entrainment parameters
+        "entrainment_factor" => [0.13],
+        "detrainment_factor" => [0.51],
+        "turbulent_entrainment_factor" => [0.075],
+        "entrainment_smin_tke_coeff" => [0.3],
+        "updraft_mixing_frac" => [0.25],
+        "entrainment_scale" => [4.0e-4],
+        "sorting_power" => [2.0],
+    )
+
+    config["unconstrained_σ"] = 1.0
+    return config
+end
+
+function get_scm_config()
+    config = Dict()
+    config["namelist_args"] = [
+        ("time_stepping", "dt_min", 1.0),
+        ("time_stepping", "dt_max", 2.0),
+        ("stats_io", "frequency", 60.0),
+        ("grid", "dz", 50.0),
+        ("grid", "nz", 80),
+    ]
+    return config
+end
