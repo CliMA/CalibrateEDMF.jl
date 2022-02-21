ad5a14f5df4a9b153df1afb6fc7f7969e976d7bf
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-02-21 14:29:59 -0800

    Add GHA tests back in, make bors not depend on GHA

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
-using Test
 
-@testset "Unit tests" begin
-    @test 1 == 1
-end
+include(joinpath("helper_funcs", "runtests.jl"))
+include(joinpath("DistributionUtils", "runtests.jl"))
+include(joinpath("LESUtils", "runtests.jl"))
+include(joinpath("TurbulenceConvectionUtils", "runtests.jl"))
+include(joinpath("ReferenceModels", "runtests.jl"))
+include(joinpath("ReferenceStats", "runtests.jl"))
+include(joinpath("Pipeline", "runtests.jl"))
+include(joinpath("NetCDFIO", "runtests.jl"))
