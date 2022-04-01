3ddb2c4b8078e51b816fbfccbe9f971788392d59
yairchn <yairchn@caltech.edu>
2022-04-01 15:46:50 -0700

    bug fix in parameter values and sme name changes

--- experiments/grid_search/loss_map.jl
+++ experiments/grid_search/loss_map.jl
@@ -18,58 +18,53 @@ function compute_loss_map(config, sims_path, sim_type)
     @assert sim_type in ("reference", "validation")
     n_ens = get_entry(config["grid_search"], "ensemble_size", nothing)
     case_names = get_entry(config[sim_type], "case_name", nothing)
+    n_cases = length(case_names)
 
     # param_paths = filter(isdir, readdir(sims_path, join=true))
     parameters = get_entry(config["grid_search"], "parameters", nothing)
     param_names = collect(keys(parameters))
     NC.Dataset(joinpath(sims_path, "loss_hypercube.nc"), "c") do ds
-        for (param1, param2) in combinations(param_names, 2)
-            group_name = "$param1.$param2"
+        for (param_name_1, param_name_2) in combinations(param_names, 2)
+            group_name = "$param_name_1.$param_name_2"
             NC.defGroup(ds, group_name, attrib = [])
-            A = parameters[param1]
-            B = parameters[param2]
-            param_values = vec(collect(Iterators.product(A, B)))
 
             # iterate all folders, e.g. "0.1_0.2", ...
             # compute loss, store in matrix:
             # (case, ens_i, p1, p2)
-            pval1 = first.(param_values)
-            pval2 = last.(param_values)
+            param_val_1 = parameters[param_name_1]
+            param_val_2 = parameters[param_name_2]
+            n_param_val_1 = length(param_val_1)
+            n_param_val_2 = length(param_val_2)
 
-            n_pval1 = length(pval1)
-            n_pval2 = length(pval2)
+            loss_2D_sec = zeros((n_param_val_1, n_param_val_2, n_cases, n_ens))
 
-            n_cases = length(case_names)
+            nt = (; param_val_1, param_val_2, sims_path, group_name, config, sim_type)
 
-            loss = zeros((n_pval1, n_pval2, length(case_names), n_ens))
-
-            nt = (; pval1, pval2, sims_path, group_name, config, sim_type)
-
-            loss_configs = vec(collect(Iterators.product(1:n_pval1, 1:n_pval2, 1:n_cases, 1:n_ens, [nt])))
+            loss_configs = vec(collect(Iterators.product(1:n_param_val_1, 1:n_param_val_2, 1:n_cases, 1:n_ens, [nt])))
 
             @time begin
                 sim_loss = pmap(compute_loss, loss_configs)
-                loss = reshape(sim_loss, size(loss))
+                loss_2D_sec = reshape(sim_loss, size(loss_2D_sec))
             end
             ensemble_member = LinRange(1, n_ens, n_ens)
             group_root = ds.group[group_name]  # group is e.g. 'sorting_power.entrainment_factor'
-            # Define dimensions: param1, param2, case, ensemble_member
-            NC.defDim(group_root, "param1", n_pval1)
-            NC.defDim(group_root, "param2", n_pval2)
-            NC.defDim(group_root, "case", length(case_names))
+            # Define dimensions: param_name_1, param_name_2, case, ensemble_member
+            NC.defDim(group_root, "param_name_1", n_param_val_1)
+            NC.defDim(group_root, "param_name_2", n_param_val_2)
+            NC.defDim(group_root, "case", n_cases)
             NC.defDim(group_root, "ensemble_member", n_ens)
             # Define variables: 
             ncvar = NC.defVar(group_root, "case", case_names, ("case",))
             ncvar[:] = case_names  # "Bomex", "DYCOMS"
             ncvar = NC.defVar(group_root, "ensemble_member", ensemble_member, ("ensemble_member",))
             ncvar[:] = ensemble_member  # 1, 2, ..., n_ens
-            # param1 and param2 values
-            ncvar = NC.defVar(group_root, param1, pval1, ("param1",))
-            ncvar[:] = pval1
-            ncvar = NC.defVar(group_root, param2, pval2, ("param2",))
-            ncvar[:] = pval2
-            ncvar = NC.defVar(group_root, "loss_data", loss, ("param1", "param2", "case", "ensemble_member"))
-            ncvar[:, :, :, :] = loss
+            # param_name_1 and param_name_2 values
+            ncvar = NC.defVar(group_root, param_name_1, param_val_1, ("param_name_1",))
+            ncvar[:] = param_val_1
+            ncvar = NC.defVar(group_root, param_name_2, param_val_2, ("param_name_2",))
+            ncvar[:] = param_val_2
+            ncvar = NC.defVar(group_root, "loss_data", loss_2D_sec, ("param_name_1", "param_name_2", "case", "ensemble_member"))
+            ncvar[:, :, :, :] = loss_2D_sec
         end
     end  # do-block
 end
@@ -77,23 +72,23 @@ end
 @everywhere begin
 """
     compute_loss(t) = compute_loss(t...)
-    compute_loss(param1_ind, param2_ind, case_ind, ens_ind, nt)
+    compute_loss(param_name_1_ind, param_name_2_ind, case_ind, ens_ind, nt)
 
 Compute loss function from forward model output data.
 
 Arguments:
-param1_ind  :: index of a parameter value (in config > grid_search > parameters > param1)
-param2_ind  :: index of a parameter value (in config > grid_search > parameters > param2)
+param_name_1_ind  :: index of a parameter value (in config > grid_search > parameters > param_name_1)
+param_name_2_ind  :: index of a parameter value (in config > grid_search > parameters > param_name_2)
 case_ind    :: index of a case name (in config > reference/validation > case_name)
 ens_ind     :: index of the ensemble member
 nt          :: Named tuple that contains information that is constant across forward model data.
-    Has the entries `pval1`, `pval2`, `sims_path`, `group_name`, `config`, `sim_type`, denoting
-    the values for param1, the values for param2, the root path to forward model data, the 
+    Has the entries `param_val_1`, `param_val_2`, `sims_path`, `group_name`, `config`, `sim_type`, denoting
+    the values for param_name_1, the values for param_name_2, the root path to forward model data, the 
     parameter pair name, the config file, and the simulation type (reference or validation),
     respectively.
 """
 compute_loss(t) = compute_loss(t...)
-function compute_loss(param1_ind::I, param2_ind::I, case_ind::I, ens_ind::I, nt::NamedTuple) where {I <: Integer}
+function compute_loss(param_name_1_ind::I, param_name_2_ind::I, case_ind::I, ens_ind::I, nt::NamedTuple) where {I <: Integer}
     sim_type = nt.sim_type
 
     n_ens = get_entry(nt.config["grid_search"], "ensemble_size", nothing)
@@ -104,8 +99,8 @@ function compute_loss(param1_ind::I, param2_ind::I, case_ind::I, ens_ind::I, nt:
     y_dir = get_entry(nt.config[sim_type], "y_dir", nothing)
     y_ref_type = get_entry(nt.config[sim_type], "y_reference_type", nothing)
 
-    p1 = nt.pval1[param1_ind]
-    p2 = nt.pval2[param2_ind]
+    p1 = nt.param_val_1[param_name_1_ind]
+    p2 = nt.param_val_2[param_name_2_ind]
     param_path = joinpath(nt.sims_path, nt.group_name, "$(p1)_$(p2)")
     case_name = case_names[case_ind]
     sim_path = joinpath(param_path, "$case_name/Output.$case_name.$ens_ind")
