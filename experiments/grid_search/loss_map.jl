a7945d0afa41ad24aa74132e9929555592dc4b5e
yairchn <yairchn@caltech.edu>
2022-04-11 16:41:43 -0700

    cleanups, add run_loss_map.sbatch

--- experiments/grid_search/loss_map.jl
+++ experiments/grid_search/loss_map.jl
@@ -18,76 +18,62 @@ using Distributed
         sim_type = "reference",
         sims_path = "output/20220328_bNQ/",
     )
-
         config = get_config()
-
         n_ens = get_entry(config["grid_search"], "ensemble_size", nothing)
         case_names = get_entry(config[sim_type], "case_name", nothing)
-        loss_names = get_entry(config[sim_type], "y_names", nothing)
-        t_start = get_entry(config[sim_type], "t_start", nothing)
-        t_end = get_entry(config[sim_type], "t_end", nothing)
-        y_dir = get_entry(config[sim_type], "y_dir", nothing)
-        y_ref_type = get_entry(config[sim_type], "y_reference_type", nothing)
 
-        # getparam combinations
         # param_paths = filter(isdir, readdir(sims_path, join=true))
         parameters = get_entry(config["grid_search"], "parameters", nothing)
         param_names = collect(keys(parameters))
         NC.Dataset(joinpath(sims_path,"loss_hypercube.nc"),"c") do ds
-            @time begin
-                for (param1, param2) in combinations(param_names, 2)
-                    group_name = "$param1.$param2"
-                    NC.defGroup(ds, group_name, attrib = [])
-                    A = parameters[param1]
-                    B = parameters[param2]
-                    param_values = vec(collect(Iterators.product(A, B)))
+            for (param1, param2) in combinations(param_names, 2)
+                group_name = "$param1.$param2"
+                NC.defGroup(ds, group_name, attrib = [])
+                A = parameters[param1]
+                B = parameters[param2]
+                param_values = vec(collect(Iterators.product(A, B)))
 
-                    # iterate all folders, e.g. "0.1_0.2", ...
-                    # compute loss, store in matrix: 
-                    # (case, ens_i, p1, p2)
-                    pval1 = first.(param_values)
-                    pval2 = last.(param_values)
+                # iterate all folders, e.g. "0.1_0.2", ...
+                # compute loss, store in matrix:
+                # (case, ens_i, p1, p2)
+                pval1 = first.(param_values)
+                pval2 = last.(param_values)
 
-                    n_pval1 = length(pval1)
-                    n_pval2 = length(pval2)
+                n_pval1 = length(pval1)
+                n_pval2 = length(pval2)
 
-                    n_cases = length(case_names)
+                n_cases = length(case_names)
 
-                    loss = zeros((n_pval1, n_pval2, length(case_names), n_ens))
+                loss = zeros((n_pval1, n_pval2, length(case_names), n_ens))
 
-                    nt = (; pval1, pval2, sims_path, group_name, config, sim_type)
+                nt = (; pval1, pval2, sims_path, group_name, config, sim_type)
 
-                    loss_configs = vec(collect( Iterators.product(1:n_pval1, 1:n_pval2, 1:n_cases, 1:n_ens, [nt]) ))
+                loss_configs = vec(collect( Iterators.product(1:n_pval1, 1:n_pval2, 1:n_cases, 1:n_ens, [nt]) ))
 
-                    @time begin
-                        sim_loss = pmap(compute_loss, loss_configs)
-                        loss = reshape(sim_loss, size(loss))
-                    end
-
-                    @time begin
-                        # yair - need to create a group for each loss map and store the valus and coordinates
-                        ensemble_member = LinRange(1, n_ens, n_ens)
-                        group_root = ds.group[group_name]  # group is 'sorting_power.entrainment_factor'
-                        # Define dimensions: param1, param2, case, ensemble_member
-                        NC.defDim(group_root, "param1", n_pval1)
-                        NC.defDim(group_root, "param2", n_pval2)
-                        NC.defDim(group_root, "case", length(case_names))
-                        NC.defDim(group_root, "ensemble_member", n_ens)
-                        # Define variables: 
-                        ncvar = NC.defVar(group_root, "case", case_names, ("case",))
-                        ncvar[:] = case_names  # "Bomex", "DYCOMS"
-                        ncvar = NC.defVar(group_root, "ensemble_member", ensemble_member, ("ensemble_member",))
-                        ncvar[:] = ensemble_member  # 1, 2, ..., n_ens
-                        # param1 and param2 values
-                        ncvar = NC.defVar(group_root, param1, pval1, ("param1",))
-                        ncvar[:] = pval1
-                        ncvar = NC.defVar(group_root, param2,pval2,("param2",))
-                        ncvar[:] = pval2
-                        ncvar = NC.defVar(group_root, "loss_data", loss, ("param1", "param2", "case", "ensemble_member"))
-                        ncvar[:,:,:,:] = loss
-                    end
+                @time begin
+                    sim_loss = pmap(compute_loss, loss_configs)
+                    loss = reshape(sim_loss, size(loss))
                 end
-            end  # time
+                ensemble_member = LinRange(1, n_ens, n_ens)
+                group_root = ds.group[group_name]  # group is 'sorting_power.entrainment_factor'
+                # Define dimensions: param1, param2, case, ensemble_member
+                NC.defDim(group_root, "param1", n_pval1)
+                NC.defDim(group_root, "param2", n_pval2)
+                NC.defDim(group_root, "case", length(case_names))
+                NC.defDim(group_root, "ensemble_member", n_ens)
+                # Define variables: 
+                ncvar = NC.defVar(group_root, "case", case_names, ("case",))
+                ncvar[:] = case_names  # "Bomex", "DYCOMS"
+                ncvar = NC.defVar(group_root, "ensemble_member", ensemble_member, ("ensemble_member",))
+                ncvar[:] = ensemble_member  # 1, 2, ..., n_ens
+                # param1 and param2 values
+                ncvar = NC.defVar(group_root, param1, pval1, ("param1",))
+                ncvar[:] = pval1
+                ncvar = NC.defVar(group_root, param2,pval2,("param2",))
+                ncvar[:] = pval2
+                ncvar = NC.defVar(group_root, "loss_data", loss, ("param1", "param2", "case", "ensemble_member"))
+                ncvar[:,:,:,:] = loss
+            end
         end  # do-block
     end
 
